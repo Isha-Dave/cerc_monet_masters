cf41d474d543b233ae6ec8b49a9b9d65d30f34e0
kartheemalireddy <159695672+kartheemalireddy@users.noreply.github.com>
2024-02-11 12:17:25 +0000

    Karthee's Square  assignment
    program to move the robo in a square shape with 50 cm

---
+++ RobotMoveSquare_Karthee.py
@@ -0,0 +1,57 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+#Initialisate the robot 
+# Step 1 - Initialize the drive base 
+# Step 2 - Initialize the front and back motors 
+# Step 3 - Initialize the left and right color sensors in the front
+#---------------------------------------------------------------------
+
+# Initialize both motors. In this example, the motor on the
+# left must turn counterclockwise to make the robot go forward.
+left_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.A)
+
+# Initialize the drive base. 
+# In our robot, the wheel diameter is 88mm.
+# The distance between the two wheel-ground contact points is 145mm.
+drive_base = DriveBase(left_motor, right_motor, wheel_diameter=88, axle_track=145)
+# Optionally, uncomment the line below to use the gyro for improved accuracy.
+drive_base.use_gyro(True)
+
+#Initialize the front abnd back motors 
+front_motor = Motor(Port.D)
+back_motor = Motor(Port.C)
+
+# Initialize the color sensors.
+right_sensor = ColorSensor(Port.F)
+left_sensor = ColorSensor(Port.E)
+
+
+#------------------------------------------
+# Code for the robot
+#------------------------------------------
+
+
+def SquareMove():
+    
+    #moves forward 50cm
+    drive_base.straight(500)
+    #turns right 90 degrees
+    drive_base.turn(90) 
+    #moves forward 50cm
+    drive_base.straight(500)
+    #turns right 90 degrees
+    drive_base.turn(90)
+    #moves forward 50cm
+    drive_base.straight(500)
+    #turns right 90 degrees
+    drive_base.turn(90)
+    #moves forward 50cm
+    drive_base.straight(500)
+
+SquareMove()
